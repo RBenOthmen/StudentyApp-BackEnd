0f4580f584b176be9074cb5d187fba1894cc1275
BenOthmenR <ramibenothmen15@gmail.com>
2024-01-30 16:34:02 +0100

    Add project files.

---
+++ StudentyApp-BackEnd/Controllers/EventController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentyApp_BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<List<Event>>> GetEvents()
+        {
+            return new List<Event>
+            {
+                new Event
+                {
+                    Name = "Event 1",
+                    FirstName = "Test_FN 1",
+                    LastName = "Test_LN 1",
+                    Place = "Place 1"
+                }
+            };
+        }
+    }
+}
